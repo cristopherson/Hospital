45de6ce21cdb2a3ebddbefe00c839f277417eefd
Cristopherson Torres <cristopherson.torres@gmail.com>
2014-05-04 13:35:18 -0500

    Add a sleep to avoid eating too much CPU

--- src/middleware/MiddlewareListenerThread.java
+++ src/middleware/MiddlewareListenerThread.java
@@ -1,5 +1,8 @@
 package middleware;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MiddlewareListenerThread implements Runnable{
 	private Middleware middleware;
 
@@ -9,8 +12,14 @@ public class MiddlewareListenerThread implements Runnable{
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		while(true)
-			middleware.txMemberList();
+		while(true) {
+                    try {
+                        Thread.sleep(5);
+                        middleware.txMemberList();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(MiddlewareListenerThread.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
 	}
 
 }
